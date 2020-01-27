467f4fec053f671a1ffbea56056d3525ae92f4ff
andre <andre@labtime.ufg.br>
2020-01-27 15:57:59 -0300

    post create and list

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -7,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCreateComponent implements OnInit {
 
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter();
 
   ondAddPost() {
-    // alert('Post added!');
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 
   constructor() { }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  // posts = [
+  //   { title: 'First Post', content: 'This is first post\s content' },
+  //   { title: 'Second Post', content: 'This is second post\s content' },
+  //   { title: 'Third Post', content: 'This is third post\s content' }
+  // ];
+  @Input() posts = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
